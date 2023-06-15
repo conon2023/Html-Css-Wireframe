5f5bb95d6cf8298bdacbcec2344a4e4001198f44
CHIDI ONONYE <fononye2020@gmail.com>
2023-06-15 16:21:32 +0100

    removed extra files

---
+++ app.js
@@ -0,0 +1,14 @@
+//Arithmetic Operations
+let addition = 7+7 // + for addition
+console.log(addition)
+let subtraction = 5-2 // - for subtraction
+console.log(subtraction)
+let multiplication = 10*10 // * is for multiplication
+console.log(multiplication)
+let division = 15/3 // / is for division
+console.log(division)
+let userName = prompt("Hello User, please tell me your name");
+console.log(userName);
+
+const welcomeMsg = alert("Welcome to my page " + userName)
+
