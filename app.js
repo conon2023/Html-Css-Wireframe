bb73dbbd265f589e0a7b8a5acf7e14c9a3c63a2a
CHIDI ONONYE <fononye2020@gmail.com>
2023-06-16 08:53:55 +0100

    made changes using dynamic component to html page

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-//Arithmetic Operations
-let addition = 7+7 // + for addition
-console.log(addition)
-let subtraction = 5-2 // - for subtraction
-console.log(subtraction)
-let multiplication = 10*10 // * is for multiplication
-console.log(multiplication)
-let division = 15/3 // / is for division
-console.log(division)
+
+// Dynamic Component 1: Get user Input through a form
 let userName = prompt("Hello User, please tell me your name");
 console.log(userName);
+alert("Welcome to my page " + userName);
 
-const welcomeMsg = alert("Welcome to my page " + userName)
+document.addEventListener('DOMContentLoaded', function() {
+    var nameButton = document.getElementById('nameButton');
+    nameButton.addEventListener('click', function() {
+
+
+
+// Dynamic Component 2: Conditional Statement - Click a button and get a message response
+      var userInput = prompt('Enter your name:');
+      
+      if (userInput === 'John') {
+        alert('Hello John! You clicked the button.');
+      } else {
+        alert('Hello ' + userInput + '! You clicked the button.');
+      }
+    });
+  });
+
+
+// Dynamic Component 3: Conditional Statement - Change color on button click
+const colorButton = document.getElementById('colorButton');
+const colorBox = document.getElementById('colorBox');
+
+colorButton.addEventListener('click', function() {
+  const randomNumber = Math.random();
+  const threshold = 0.5;
+  if (randomNumber < threshold) {
+    colorBox.style.backgroundColor = 'red';
+  } else {
+    colorBox.style.backgroundColor = 'blue';
+  }
+});
 
