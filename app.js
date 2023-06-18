406960e16b153ab413b12ebd8ef4ca9304059dce
CHIDI ONONYE <fononye2020@gmail.com>
2023-06-18 17:42:31 +0100

    had to make another one when I could not see where the other was uploaded on git hub

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
+// let userName = prompt("Hello User, please tell me your name");
+// console.log(userName);
+// alert("Welcome to my page " + userName);
 
-// Dynamic Component 1: Get user Input through a form
-let userName = prompt("Hello User, please tell me your name");
-console.log(userName);
-alert("Welcome to my page " + userName);
+// document.addEventListener('DOMContentLoaded', function() {
+//   var nameButton = document.getElementById('nameButton');
+//   nameButton.addEventListener('click', function() {
+//     var userInput = prompt('Enter your name:');
 
-document.addEventListener('DOMContentLoaded', function() {
-    var nameButton = document.getElementById('nameButton');
-    nameButton.addEventListener('click', function() {
+//     while (!userInput) {
+//       alert('Please enter a valid name.');
+//       userInput = prompt('Enter your name:');
+//     }
 
+//     if (userInput === 'John') {
+//       alert('Hello John! You clicked the button.');
+//     } else {
+//       alert('Hello ' + userInput + '! You clicked the button.');
+//     }
+//   });
 
+//   var colorButton = document.getElementById('colorButton');
+//   var colorBox = document.getElementById('colorBox');
 
-// Dynamic Component 2: Conditional Statement - Click a button and get a message response
-      var userInput = prompt('Enter your name:');
-      
-      if (userInput === 'John') {
-        alert('Hello John! You clicked the button.');
-      } else {
-        alert('Hello ' + userInput + '! You clicked the button.');
-      }
-    });
-  });
+//   colorButton.addEventListener('click', function() {
+//     var randomColor = getRandomColor();
+//     colorBox.style.backgroundColor = randomColor;
+//   });
 
-
-// Dynamic Component 3: Conditional Statement - Change color on button click
-const colorButton = document.getElementById('colorButton');
-const colorBox = document.getElementById('colorBox');
-
-colorButton.addEventListener('click', function() {
-  const randomNumber = Math.random();
-  const threshold = 0.5;
-  if (randomNumber < threshold) {
-    colorBox.style.backgroundColor = 'red';
-  } else {
-    colorBox.style.backgroundColor = 'blue';
-  }
-});
-
+//   function getRandomColor() {
+//     var letters = '0123456789ABCDEF';
+//     var color = '#';
+//     for (var i = 0; i < 6; i++) {
+//       color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+//   }
+// });
